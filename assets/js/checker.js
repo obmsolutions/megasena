a814b64fa2864d95fcc0006e393805cdfdba5037
obmsolutions <163904426+obmsolutions@users.noreply.github.com>
2026-08-22 12:05:06 -0300

    Delete assets directory

--- assets/js/checker.js
+++
@@ -1,5 +0,0 @@
-(function(global){
-  'use strict';
-  function process(combinations,draw){const winning=new Set(draw.listaDezenas.map(Number));return combinations.map(numbers=>({numbers,matches:numbers.filter(n=>winning.has(n)).length,drawNumbers:[...winning]}));}
-  global.OBMChecker={process,parse:OBMCore.parseCombinations};
-})(window);
